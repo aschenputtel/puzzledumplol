14572a04227100fdcaa2e1ef70c21a7f757eb624
Lissa <pshycosloth@gmail.com>
2014-12-20 13:20:19 -0500

    added a thing

--- side1.js
+++ side1.js
@@ -1,12 +1,15 @@
 
 function passPrompt() {
-    var answer = prompt("Please enter the password to access code:");
+    var answer = prompt("Please enter the pin to access flag:");
     
     if (answer == '2239690868') {
         document.getElementById("textchange").innerHTML = 'You got the answer right! Take the code "c6h12o6" back to IceQueenMiki for your free graphic! :D';
     }
     else if (answer == null){
     	document.getElementById("textchange").innerHTML = 'At least try, dangit!';
+    }
+    else if (answer.length < 5){
+        document.getElementById("textchange").innerHTML = 'Try a larger number.';
     }
     else{
     	document.getElementById("textchange").innerHTML = 'Wrong answer! Take the code "sugar" back to IceQueenMiki for a hint!';

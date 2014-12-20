d9fad8851780961605a119e2a70358688d8d158d
Lissa <pshycosloth@gmail.com>
2014-12-20 08:56:00 -0500

    ..

--- side1.js
+++ side1.js
@@ -5,6 +5,9 @@ function passPrompt() {
     if (answer == '2239690868') {
         document.getElementById("textchange").innerHTML = 'You got the answer right! Take the code "c6h12o6" back to IceQueenMiki for your free graphic! :D';
     }
+    else if (answer == null){
+    	document.getElementById("textchange").innerHTML = 'At least try, dangit!';
+    }
     else{
     	document.getElementById("textchange").innerHTML = 'Wrong answer! Take the code "sugar" back to IceQueenMiki for a hint!';
     }
